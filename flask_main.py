0d5f36395825ed98dba7915f7e0f085c257db900
Johan <JKOK005@e.ntu.edu.sg>
2016-08-13 22:58:51 +0800

    Modified flask file

--- flask_main.py
+++ flask_main.py
@@ -6,3 +6,14 @@ import os
 
 app 				= Flask(__name__)
 app.debug 			= True
+
+@app.route('/<department>/<timestamp>', methods=['GET','POST'])
+def show_garden(department, timestamp):
+	return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def show_tmp():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+	app.run()
